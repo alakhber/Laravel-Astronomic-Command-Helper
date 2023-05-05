b9825aa1ed736690c84043e6a58014b5018449ec
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-05-05 18:07:46 +0400

    generic command class

---
+++ app/Utilities/Classes/Commands/TranslateCommand.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Utilities\Classes\Commands;
+
+class TranslateCommand {
+
+    private $tableName;
+    private $argument;
+    private $consoleOutput;
+    private $columns;
+    private $namespace = '\App\Models\\';
+
+   
+    public function __construct()
+    {
+        $this->consoleOutput = new \Symfony\Component\Console\Output\ConsoleOutput();
+    }
+
+    public function run($argument){
+        if (empty($argument)){
+            $this->consoleOutput->writeln("<error>Argument Not Found !</error>");
+            die;
+        } 
+
+        $modelFile = app_path('Models/' . ucfirst($argument) . '.php');
+        if (!file_exists($modelFile)) {
+            $this->consoleOutput->writeln("<error>" . ucfirst($argument) . " Model Not Found !</error>");
+        }
+
+        $this->argument = $argument;
+        $this->tableName = strtolower($this->argument);
+        $this->makeMigration();
+    }
+
+    private function makeModel(){
+        
+    }
+
+    private function makeMigration(){
+        $this->getTranslatedAttributes();
+        return (new GenerateMigrationCommand())->run($this->argument,$this->columns);
+    }
+
+ 
+
+    private function getTranslatedAttributes()
+    {
+        $modelName = $this->namespace. ucfirst($this->tableName);
+
+        $this->columns =  (new  $modelName())->translatedAttributes;
+    }
+}
